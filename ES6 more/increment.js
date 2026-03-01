72dadc7184d6e90798c60f6181a3dbc835e70389
mdallamaiqbal <bd999plus13@gmail.com>
2026-03-01 11:29:45 +0600

    increment and decrement

---
+++ ES6 more/increment.js
@@ -0,0 +1,19 @@
+let a=5;
+a++;
+//console.log(a++);//eta dele value pore barai dekhabe call korar por;
+console.log(++a);//eta age barabe 
+console.log(a);
+/* 
+++x → pre-increment → value আগে increase, পরে use
+x++ → post-increment → value use আগে, increase পরে 
+*/
+let b=5;
+b--;
+console.log(--b);
+// console.log(b--);same
+//console.log(b);
+
+/* 
+--y → pre-decrement → value আগে decrease, পরে use
+y-- → post-decrement → value use আগে, decrease পরে
+ */
