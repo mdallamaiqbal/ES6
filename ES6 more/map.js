9c6372b7adda79cd0886ad98edd7cc16823b75a4
mdallamaiqbal <bd999plus13@gmail.com>
2026-03-01 10:12:09 +0600

    add map.js

---
+++ ES6 more/map.js
@@ -0,0 +1,31 @@
+const number=[2,4,8,20];
+// const doubled=[];
+// for(let num of number){
+//     doubled.push(num*2);
+// }
+// const doubleIT=num=>num*2;
+// const doubled=number.map(doubleIT);
+const doubled=number.map(num=>num*2);//we can use any of them
+const squared=number.map(num=>num*num)
+console.log(doubled);
+console.log(squared);
+
+const friends=['jabed','obed','kabed']
+const friend=friends.map(frd=>frd[0]);
+console.log(friend);
+
+const products=[
+    {name:'laptop',price:20000},
+    {name:'tv',price:25000},
+    {name:'charger',price:3000}
+]
+const product=products.map(pd=>pd.price*2);
+console.log(product);
+const names=products.map((pd,index,productArray)=>{
+    const upperCase=pd.name.toUpperCase();
+    console.log(upperCase,index,productArray);
+    
+    return upperCase
+});
+console.log(names);
+
